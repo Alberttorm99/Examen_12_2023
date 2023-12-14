86a615a2d7a10dec66381b1935c5eeb1f8b741ea
Alberttorm99 <a.romero.vmware@gmail.com>
2023-12-14 17:14:10 +0100

    Ejercicio

---
+++ src/modelo/FiguraGeometrica.java
@@ -0,0 +1,56 @@
+package modelo;
+
+public class FiguraGeometrica {
+
+    private int numLados;
+    private String nombre;
+    private static final int MIN_NUMBER = 3;
+
+    public void showInfo() {
+        System.out.printf("Soy un %s. Tengo %d lados%n", nombre, numLados);
+    }
+
+    
+    public FiguraGeometrica(int numLados, String nombre) throws Exception {
+        if (numLados < MIN_NUMBER) {
+            throw new Exception("El número de lados debe ser mayor que " + MIN_NUMBER);
+        }
+        this.numLados = numLados;
+        this.nombre = nombre;
+    }
+/*Alberto Romero Dauzon*/
+
+
+    public int getNumLados() {
+        return numLados;
+    }
+
+
+    public FiguraGeometrica() {
+        
+    }
+
+
+    
+
+
+    public void setNumLados(int numLados) {
+        this.numLados = numLados;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+   
+
+
+}
